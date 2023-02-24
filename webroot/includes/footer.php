090a6c30daacd25da3dc257edae04de7d59eb46d
Cody Ashe-McNalley <codyam@g.ucla.edu>
2023-02-24 10:09:05 -0800

    initial commit

---
+++ webroot/includes/footer.php
@@ -0,0 +1,74 @@
+<!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-6 col-md-6">
+            <div class="footer-info">
+              <img src="assets/img/picksc-logo-enhance.png" width="150px">
+              <h5 style="color:white;">Particle-in-Cell and Kinetic Simulation Software Center</h5>
+              <p>
+                475 Portola Plaza, Los Angeles, CA 90095-1547<br><br>
+                <strong>Phone:</strong> 310-206-0372<br>
+                <strong>Email:</strong> mori@physics.ucla.edu<br>
+              </p>
+              <!--div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>-->
+            </div>
+          </div>
+
+          <!--<div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://www.pa.ucla.edu/">UCLA Physics & Astronomy</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://www.ucla.edu/">UCLA Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="Division of Physical Sciences">Division of Physical Sciences</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://covid-19.ucla.edu/">UCLA COVID-19 Resources</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="http://www.ucla.edu/maps-directions-parking/">Maps, directions, parking</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Software</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Item One</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Item Two</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Item Three</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Item Four</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Item Five</a></li>
+            </ul>
+          </div>-->
+
+          <div class="col-lg-6 col-md-6">
+            <div class="footer-info">
+              <h4>Sponsors</h4>
+              <p>Funded by &nbsp;
+              <img src="assets/img/nsf-transp.png" width="75px"</p>
+              <p>Some activities supported by <br /> <img src="assets/img/scidac-transp.png" width="300px"
+              
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+	<hr>
+    <div class="container">
+      <div class="copyright">
+      	<p style="color:#FFFFFF";>&copy; Copyright 2021 <strong><span>PICKSC</span></strong>. Designed by https://bootstrapmade.com/</p>
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/sailor-free-bootstrap-theme/ -->
+        
+      </div>
+    </div>
+  </footer><!-- End Footer -->
---
+++ webroot/includes/menu.php
@@ -0,0 +1,83 @@
+<!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+      <!--<h1 class="logo me-auto"><a href="index.html">UCLA PICKSC Logo</a></h1>-->
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <a href="index.html"><img src="assets/img/picksc-transp.png" width="100px" alt="" class="img-fluid"></a>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="index.html" class="active">Home</a></li>
+          <li><a href="about-us.html">About Us</a></li>
+          <li><a href="team.html">Team</a></li>
+
+          <li class="dropdown"><a href="#"><span>Research</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="research-overview.html">Overview</a></li>
+              <li><a href="publications.html">Publications</a></li>
+              <!--<li><a href="#">Testimonials</a></li>-->
+
+              <!--<li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>-->
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Software</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="software-overview.html">Overview</a></li>
+              <li><a href="production-codes.html">Production Codes</a></li>
+              <li><a href="skeleton-code.html">Skeleton Codes</a></li>
+              <li><a href="upic-framework.html">UPIC Framework</a></li>
+              <lI><a href="gridless-particle-codes.html">Gridless Particle Codes</a></li>
+              <li><a href="fortran-2003-techniques.html">Fortran 2003 Techniques</a></li>
+              <!--<li><a href="#">Testimonials</a></li>-->
+
+              <!--<li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>-->
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Education</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="education-overview.html">Overview</a></li>
+              <li><a href="jupyterPIC.html">JupyterPIC</a></li>
+              <li><a href="particle-orbit-visualization.html">Particle Orbit Visualization</a></li>
+              <li><a href="python-pic-gui.html">Python PIC GUI</a></a></li>
+              <li><a href="zpic.html">ZPIC</a></li>
+              <!--<li><a href="#">Testimonials</a></li>-->
+
+              <!--<li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>-->
+            </ul>
+          </li>
+          <li><a href="news.html">News</a></li>
+          <li><a href="opportunities.html">Opportunities</a></li>
+
+          <li><a href="mailto:mori@physics.ucla.edu">Contact</a></li>
+          <!--<li><a href="index.html" class="getstarted">Get Started</a></li>-->
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
